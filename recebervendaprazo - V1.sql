83410887db1244b131892ae22fdfccd984265462
werlleymendes <werlleymendes@hotmail.com>
2021-08-27 19:04:57 -0300

    Conclusão da consulta da tabela recebervendaprazo v1

---
+++ recebervendaprazo - V1.sql
@@ -0,0 +1,63 @@
+/*CONSULTA PARA MIGRAÇÃO DO BANCO DE DADOS DO VR PARA O C5*/
+/*consulta para migração do vr para a consinco 
+contas a receber, tabela recebervendaprazo*/
+
+SELECT null as "SeqIntTitulo", 1 as "TipoRegistro", 1 as "NroEmpresaMae",
+CASE 
+when rv.id_loja = 1 then 1
+when rv.id_loja = 2 then 2
+when rv.id_loja = 4 then 3
+end as "NroEmpresa",
+'DUPR' as "CodEspecie", null as "TipoCodGerente", 
+null as "CodGerente",null as "TipoCodVendedor", 
+null as "CodVendedor", 2 as "TipoCodPessoa", 
+LEFT(CAST(ce.cnpj as VARCHAR), -2) as "CodPessoa",
+2 as "TipoCodPessoaNota",
+LEFT(CAST(ce.cnpj as VARCHAR), -2) as "CodPessoaNota",
+rv.id as "NroTitulo", null as "SerieTitulo", 1 as "NroParcela",
+rv.numeronota as "NroDocumento", null as "SerieDoc", 
+(rv.valor - coalesce(rvi.valor, 0)) as "VlrOriginal",
+rv.dataemissao as "DtaEmissao", rv.datavencimento as "DtaVencimento", 
+'P' as "TipoVencOriginal", null as "DtaProgramada", 
+(rv.id || ', ' || coalesce(rv.observacao, null)) as "Observacao",
+null as "CodigoFator", 'S' as "TituloEmitido", 0::float as "VlrDscFinanc", 
+0::float as "PctDescFinanc", rv.datavencimento as "DtaLimDscFinanc",
+'S' as "CodCarteira", null as "Banco", null as "Agencia",
+null as "NroCompensacao", null as "CtaEmitente", 'P' as "ProprioTerceiro", 
+0::float as "SeqCtaCorrente", null as "NossoNumero", null as "DigNossoNum", 'T' as "TituloCaixa",
+'BLT' as "TipoNegociacao",
+null as "NroCarga", 0 as "PerJuroMora", rv.dataemissao as "DtaMovimento",
+null as "OrdenaCarga", null as "DtaCarga", 'J' as "Situacao", 
+null as "Origem", 
+/*Quando for rodar o insert, solicitar ajuda ao analista da TOTVS*/
+'' as "SeqTitulo", 1 as "SeqDepositario", 
+/*Quando for rodar o insert, solicitar ajuda ao analista da TOTVS*/
+'' as "NroProcesso", 'ORCAMENTO' as "UsuAlteracao", null as "IndReplicacao", 
+null as "IndGerouReplicacao", 0::float as "VlrDescComercial",  null as "SitCheqDev",
+null as "CodBarra", null as "LotePagto", 'N' as "SitJuridica", null as "Percadministracao",
+null as "Vlradministracao", 
+null as "Dtacontabilizacao", null as "Linkerp", null as "Qtdparcela", 
+null as "Vlrcomissaofat", null as "Seqboleto", null as "Seqmotorista", 
+null as "Percdesccontrato", null as "Vlrdesccontrato", null as "Justcancel", 
+null as "CMC7", null as "Apporigem"
+FROM recebervendaprazo  as rv
+JOIN clienteeventual as ce
+ON rv.id_clienteeventual = ce.id
+LEFT JOIN (SELECT rv.id as id, sum(rvi.valor) as valor FROM recebervendaprazo rv, 
+		   recebervendaprazoitem rvi 
+WHERE rv.id = rvi.id_recebervendaprazo
+AND id_situacaorecebervendaprazo = 0
+GROUP BY 1) as rvi 
+ON rv.id = rvi.id
+WHERE rv.id_situacaorecebervendaprazo = 0;
+
+
+SELECT * FROM recebervendaprazo WHERE id_situacaorecebervendaprazo = 0;
+SELECT * FROM recebervendaprazoitem;
+
+SELECT rv.id as id, sum(rvi.valor) FROM recebervendaprazo rv, recebervendaprazoitem rvi 
+WHERE rv.id = rvi.id_recebervendaprazo
+AND id_situacaorecebervendaprazo = 0
+GROUP BY 1;
+
+
