1714ffdd99d592a76a508fb33c6dfcfb0dbac4c9
werlleymendes <werlleymendes@hotmail.com>
2021-08-31 16:43:08 -0300

    Correção de problemas na query Contas a Pagar

--- Contas_a_pagar.sql
+++ Contas_a_pagar.sql
@@ -1,8 +1,10 @@
 /*CONSULTA PARA MIGRAÇÃO DO BANCO DE DADOS DO VR PARA O C5*/
 /*CONTAS A PAGAR*/
 
-SELECT null as "SeqIntTitulo", 1 as "TipoRegistro", 1 as "NroEmpresaMae",
-/*A nossa loja 4 no VR será a loja N°3 no consinco, por isso a necessidade de mudar na query */
+/*CONSULTA UTILIZADA PARA FAZER INSERT NA TABELA Tabela FI_IntTitulo*/
+SELECT 
+null as "SeqIntTitulo", 1 as "TipoRegistro", 1 as "NroEmpresaMae",
+--A nossa loja 4 no VR será a loja N°3 no consinco, por isso a necessidade de mudar na query 
 CASE 
 when pf.id_loja = 1 then 1
 when pf.id_loja = 2 then 2
@@ -23,13 +25,16 @@ left(cast(f.cnpj as varchar(14)),-2) as "CodPessoa",
 left(cast(f.cnpj as varchar(14)),-2) as "CodPessoaNota",
 pf.numerodocumento AS "NroTitulo", ne.serie AS "SerieTitulo", pff.numeroparcela AS "NroParcela", 
 ne.numeronota AS "NroDocumento", ne.serie AS "SerieDoc",
-pff.valor AS "VlrOriginal", pf.dataemissao AS "DtaEmissao",
+(pff.valor + coalesce(pff.valoracrescimo, 0) - coalesce(pfa.valorabatimento, 0) -
+	coalesce(pfad.valoradiantamento, 0) - coalesce(pfdv.valordevolucao, 0) 
+	- coalesce(pfvb.valorverba, 0)) AS "VlrOriginal", pf.dataemissao AS "DtaEmissao",
 pff.datavencimento AS "DtaEmissao", pff.datavencimento AS "DtaVencimento",
 'P' as "TipoVencOriginal", null as "DtaProgramada", 
 /*A observação é a concatenação do número do título com a observação lançada no VR software*/
-concat_ws(', ', pf.numerodocumento, pff.observacao) as "Observacao", null as "CodigoFator",
+concat_ws('; ', pf.numerodocumento, replace(pff.observacao, ',' , '.')) as "Observacao", null as "CodigoFator",
 'S' as "TituloEmitido", 
-pfa.valor AS "VlrDscFinanc", round((pfa.valor/pff.valor)*100,2) AS "PctDescFinanc",
+--verificar se realmente não tem imposto
+0::float AS "VlrDscFinanc", 0::float AS "PctDescFinanc",
 pff.datavencimento AS "DtaLimDscFinanc", 'S' as "CodCarteira", null as "Banco", 
 null as "Agencia", '' as "NroCompensacao", null as "CtaEmitente", null as "ProprioTerceiro",
 0 as "SeqCtaCorrente", '' as "NossoNumero", null as "DigNossoNum", 'T' as "TituloCaixa",
@@ -41,22 +46,56 @@ null as "SitCheqDev", null as "CodBarra", null as "LotePagto", 'N' as "SitJuridi
 null as "Percadministracao", null as "Vlradministracao", null as "Dtacontabilizacao",
 null as "Linkerp", null as "Qtdparcela", null as "Vlrcomissaofat", null as "Seqboleto",
 null as "Seqmotorista", null as "Percdesccontrato", null as "Vlrdesccontrato",
-null as "Justcancel", null as "CMC7", null as "Apporigem"
+null as "Justcancel", null as "CMC7", null as "Apporigem" 
 FROM pagarfornecedor as pf JOIN fornecedor as f
 ON pf.id_fornecedor = f.id
 LEFT JOIN pagarfornecedorparcela AS pff
 ON pff.id_pagarfornecedor = pf.id
 LEFT JOIN notaentrada AS ne
 ON pf.numerodocumento = ne.numeronota AND pf.id_fornecedor = ne.id_fornecedor
-LEFT JOIN pagarfornecedorparcelaabatimento AS pfa
-ON pff.id = pfa.id_pagarfornecedorparcela
+LEFT JOIN (SELECT pfp.id as id, sum(pfa.valor) as valorabatimento
+FROM pagarfornecedorparcela as pfp
+JOIN pagarfornecedorparcelaabatimento as pfa
+ON pfp.id = pfa.id_pagarfornecedorparcela
+WHERE pfp.id_situacaopagarfornecedorparcela =0
+GROUP BY 1) as pfa
+ON pff.id = pfa.id
+LEFT JOIN (SELECT pfp.id as id, sum(pfad.valor) as valoradiantamento
+FROM pagarfornecedorparcela as pfp
+JOIN pagarfornecedorparcelaadiantamento as pfad
+ON pfp.id = pfad.id_pagarfornecedorparcela
+WHERE pfp.id_situacaopagarfornecedorparcela =0
+GROUP BY 1) as pfad
+ON pff.id = pfad.id
+LEFT JOIN (SELECT pfp.id as id, sum(pfdv.valor) as valordevolucao
+FROM pagarfornecedorparcela as pfp
+JOIN pagarfornecedorparceladevolucao as pfdv
+ON pfp.id = pfdv.id_pagarfornecedorparcela
+WHERE pfp.id_situacaopagarfornecedorparcela =0
+GROUP BY 1) as pfdv
+ON pff.id = pfdv.id
+LEFT JOIN (SELECT pfp.id as id, sum(pfvb.valor) as valorverba
+FROM pagarfornecedorparcela as pfp
+JOIN pagarfornecedorparcelaverba as pfvb
+ON pfp.id = pfvb.id_pagarfornecedorparcela
+WHERE pfp.id_situacaopagarfornecedorparcela =0
+GROUP BY 1) as pfvb
+ON pff.id = pfvb.id
 WHERE pff.id_situacaopagarfornecedorparcela = 0;
 
 
 
+/*Consulta utilizada para fazer insert na tabela FI_IntTituloOpe*/
+SELECT '' as "SeqIntTitulo", 16::integer  as "CodOperacao", null as "SeqOP",
+
+FROM 
 
 
-SELECT *
+
+
+--Consultas auxiliares utilizadas no desenvolvimento da query principal
+
+SELECT 
 FROM fornecedorprazo AS fp 
 WHERE fp.id_loja = 1
 AND fp.id_divisaofornecedor = 0
@@ -71,12 +110,75 @@ SELECT * FROM tiporecebimento;
 
 SELECT * FROM tipofornecedor;
 
-select * from pagarfornecedorparcelaabatimento limit 10;
+SELECT * FROM pagarfornecedorparcela limit 10;
 
+SELECT pfp.valor as "valor parcela", pfp.valoracrescimo,
+pfa.valor as abatimento, 
+pfad.valor as adiantamento, pfdv.valor as devolucao,
+pfpv.valor as verba
+FROM pagarfornecedorparcela as pfp
+LEFT JOIN pagarfornecedorparcelaabatimento as pfa
+ON pfp.id = pfa.id_pagarfornecedorparcela
+LEFT JOIN pagarfornecedorparcelaadiantamento as pfad
+ON pfp.id = pfad.id_pagarfornecedorparcela
+LEFT JOIN pagarfornecedorparceladevolucao as pfdv
+ON pfp.id = pfdv.id_pagarfornecedorparcela
+LEFT JOIN pagarfornecedorparcelaverba as pfpv
+ON pfp.id = pfpv.id_pagarfornecedorparcela;
+
+SELECT f.id as fornecedor, pfp.id, pfp.numeroparcela, pfp.datavencimento, pf.numerodocumento, (sum(pfp.valor) - sum(coalesce(pfa.valor, 0)) + 
+							sum(coalesce(pfp.valoracrescimo, 0)) -
+								   coalesce(sum(pfad.valor), 0) -
+								   coalesce(sum(pfdv.valor), 0) -
+								   coalesce(sum(pfpv.valor), 0)) as "valorliquido"
+FROM pagarfornecedorparcela as pfp
+LEFT JOIN pagarfornecedor as pf
+ON pf.id = pfp.id_pagarfornecedor
+LEFT JOIN pagarfornecedorparcelaabatimento as pfa
+ON pfp.id = pfa.id_pagarfornecedorparcela
+LEFT JOIN pagarfornecedorparcelaadiantamento as pfad
+ON pfp.id = pfad.id_pagarfornecedorparcela
+LEFT JOIN pagarfornecedorparceladevolucao as pfdv
+ON pfp.id = pfdv.id_pagarfornecedorparcela
+LEFT JOIN pagarfornecedorparcelaverba as pfpv
+ON pfp.id = pfpv.id_pagarfornecedorparcela
+JOIN fornecedor as f 
+ON pf.id_fornecedor = f.id
+WHERE pfp.id_situacaopagarfornecedorparcela = 0
+GROUP BY 5,3,2,1;
+
+select * from pagarfornecedorparcela limit 1
+
+SELECT pfp.id, pf.numerodocumento, pfp.valor, pfa.valor, pfp.valoracrescimo
+FROM pagarfornecedorparcela as pfp
+LEFT JOIN pagarfornecedor as pf
+ON pf.id = pfp.id_pagarfornecedor
+LEFT JOIN pagarfornecedorparcelaabatimento as pfa
+ON pfp.id = pfa.id_pagarfornecedorparcela
+LEFT JOIN pagarfornecedorparcelaadiantamento as pfad
+ON pfp.id = pfad.id_pagarfornecedorparcela
+WHERE pfp.id_situacaopagarfornecedorparcela = 0
+AND pf.numerodocumento = 737;
+
+SELECT * FROM pagarfornecedorparcelaabatimento WHERE id_pagarfornecedorparcela = 150571;
+
+150571
 SELECT column_name, table_name FROM information_schema.columns
 WHERE column_name LIKE '%abat%';
 
-SELECT * FROM pagarfornecedorparcela LIMIT 1000;
+SELECT * FROM pagarfornecedor;
+
+SELECT * FROM pagarfornecedorparcela WHERE id_situacaopagarfornecedorparcela =0;
+SELECT * FROM pagarfornecedorparcelaabatimento LIMIT 1000;
+SELECT * FROM pagarfornecedorparcelaadiantamento LIMIT 10;
+
+SELECT pfp.id, sum(pfa.valor)
+FROM pagarfornecedorparcela as pfp
+JOIN pagarfornecedorparcelaabatimento as pfa
+ON pfp.id = pfa.id_pagarfornecedorparcela
+WHERE pfp.id_situacaopagarfornecedorparcela =0
+GROUP BY 1;
+
 SELECT * FROM pagarfornecedor LIMIT 1000;
 SELECT * FROM situacaopagarfornecedorparcela;
 select * from tipoentrada where descricao like '%RURAI%'
@@ -122,6 +224,18 @@ WHERE pfp.id_situacaopagarfornecedorparcela = 0
 GROUP BY pf.id_fornecedor;
 
 SELECT * FROM notaentrada LIMIT 10;
+
+SELECT tablename FROM pg_tables WHERE tablename LIKE '%pagarfornecedorparcelaa%';
+
+SELECT * FROM pagarfornecedorparceladevolucao;
+SELECT * FROM pagarfornecedorparcelaverba;
+SELECT * FROM pagarfornecedorparcelacontrato;
+SELECT * FROM pagarfornecedorparcelacheque;
+SELECT * FROM pagarfornecedorparcelaabatimento;
+SELECT * FROM pagarfornecedorparcelaadiantamento;
+
+
+SELECT * FROM pagarfornecedorparcela WHERE id = 67483;
 
 SELECT
 column_name,
